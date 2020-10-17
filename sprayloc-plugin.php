73960f42035c543fe36789674cb6f8fa9c5a6583
gui2one <guillaume.rouault.fx@gmail.com>
2020-10-17 18:31:54 +0200

    added testClass.php to test composer autoload

--- sprayloc-plugin.php
+++ sprayloc-plugin.php
@@ -4,3 +4,9 @@
  * Plugin Name: Sprayloc Plugin
  * Author: gui2one
  */
+
+ require_once( plugin_dir_path( __FILE__ ) . "vendor/autoload.php");
+
+ use Inc\base\testClass;
+
+ $test_class = new testClass();
